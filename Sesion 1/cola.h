6902f8aab9704d5a11a995c73741be4b917c8366
DKNS-JCC <jorgecuadradojcc@usal.es>
2024-02-15 18:09:13 +0100

    fix gitignore

---
+++ Sesion 1/cola.h
@@ -0,0 +1,33 @@
+#ifndef __COLA_H
+#define __COLA_H
+
+#ifndef __TIPO_ELEMENTO
+#define __TIPO_ELEMENTO
+typedef int tipoElemento;
+#endif
+
+#ifndef __TIPO_CELDA
+#define __TIPO_CELDA
+
+typedef struct tipoCelda
+{
+	int elemento;
+	struct tipoCelda *sig;
+} tipoCelda;
+
+#endif
+
+typedef struct
+{
+	tipoCelda *frente, *fondo;
+} Cola;
+
+int colaCreaVacia(Cola *c);
+
+int colaVacia(Cola *c);
+
+int colaInsertaC(Cola *c, int elemento);
+
+int colaSuprime(Cola *c);
+
+#endif
