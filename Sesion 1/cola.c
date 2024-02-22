0c5b62dbdd42aa96c6de2e63f940ad420b92950b
DKNS-JCC <jorgecuadradojcc@usal.es>
2024-02-22 17:16:51 +0100

    fix 2

--- Sesion 1/cola.c
+++ Sesion 1/cola.c
@@ -3,53 +3,45 @@
 #include <errno.h>
 #include "cola.h"
 
-int colaCreaVacia(Cola *c)
-{
-    c->frente = NULL;
-    c->fondo = NULL;
-    return 0;
-}
 
-// 1=Vacia 0=No vacia
-int colaVacia(Cola *c)
-{
-    if (c->frente == NULL && c->fondo == NULL)
-        return 1;
-    else
+
+int colaCreaVacia(Cola *c){
+    if (c == NULL) return -2;
+    else if (NULL == (c->frente = c->fondo = (tipoCelda *)malloc(sizeof(tipoCelda)))) return -1;
+    else {
+        c->frente->sig = NULL;
         return 0;
+    }
 }
 
-int colaInserta(Cola *c, tipoElemento elemento)
-{
-    tipoCelda *nuevo;
-    nuevo = (tipoCelda *)malloc(sizeof(tipoCelda));
-    if (nuevo == NULL)
-        return -1;
-    nuevo->elemento = elemento;
-    nuevo->sig = NULL;
-    if (colaVacia(c))
-    {
-        c->frente = nuevo;
-        c->fondo = nuevo;
-    }
-    else
-    {
-        c->fondo->sig = nuevo;
-        c->fondo = nuevo;
-    }
+int colaVacia(Cola *c){
+    if (c == NULL || c->frente == NULL) return -1;
+    else return 0;
+}
+
+int colaInserta(Cola *c,tipoElemento elemento){
+    // Añade el elemento x en la posición a continuación del fondo,
+    // o último elemento de la cola, pasando a ser el nuevo fondo de la misma
+    tipoCelda *nuevaPosicion;
+    if (colaVacia(c) == -1) return -1;
+    if(NULL == (nuevaPosicion = malloc(sizeof(tipoCelda)))) return -2;
+    nuevaPosicion->elemento = elemento;
+    nuevaPosicion->sig = NULL;
+    c->fondo->sig = nuevaPosicion;
     return 0;
 }
 
-
-int colaSuprime(Cola *c)
-{
+tipoElemento colaSuprime(Cola *c){
+    // Elimina el elemento del frente o primer elemento de la cola,
+    // pasando a ser el nuevo frente el siguiente elemento.
+    // Devuelve el valor almacenado en esa posición.
     tipoCelda *aBorrar;
-    if (colaVacia(c))
-        return -1;
-    aBorrar = c->frente;
-    c->frente = c->frente->sig;
-    if (c->frente == NULL)
-        c->fondo = NULL;
-    free(aBorrar);
-    return 0;
+    if (colaVacia(c) != 0) return -1;
+    else {
+        aBorrar = c->frente;
+        if(aBorrar == NULL) return -2;
+        c->frente = aBorrar->sig;
+        free(aBorrar);
+        return 0;
+    }
 }
--- Sesion 1/cola.h
+++ Sesion 1/cola.h
@@ -8,26 +8,26 @@ typedef int tipoElemento;
 
 #ifndef __TIPO_CELDA
 #define __TIPO_CELDA
-
-typedef struct tipoCelda
-{
-	int elemento;
-	struct tipoCelda *sig;
-} tipoCelda;
-
+typedef struct tipoCelda {
+	tipoElemento elemento;
+	struct tipoCelda * sig;
+	} tipoCelda;
 #endif
 
-typedef struct
-{
-	tipoCelda *frente, *fondo;
+
+typedef struct {
+        tipoCelda *frente, *fondo;
 } Cola;
 
 int colaCreaVacia(Cola *c);
 
+// En vacía no sería necesario el paso por referencia,
+// se hace por mantener la misma interfaz con la implementación
+// de matrices
 int colaVacia(Cola *c);
 
-int colaInserta(Cola *c, int elemento);
+int colaInsertaC(Cola *c, tipoElemento elemento);
 
-int colaSuprime(Cola *c);
+tipoElemento colaSuprime(Cola *c);
 
 #endif
